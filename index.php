b7fecfe6e132e79b1e220d5a59302052a518f82c
Jordan Silver <jcsilver@smu.edu>
2014-06-10 11:33:34 -0500

    updated for use with website

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include "../../components/header.html" ?>
+	<script language="javascript"  type="text/javascript" src="../js/index.js"> </script> 
+</head>
+<body class="blue">
+
+<div class="row yellow">
+	<div class="twelve columns">
+		<p id="randomText">HELP IS COMING. SLOWLY. FEEL FREE TO TAKE A NAP.</p>
+	</div>
+</div>
+<html>
+<body>
+<br>
+<div class="row" id="contentArea">
+	<a href="./game1/index.php">Game 1</a><br>
+	<a href="./game2/index.php">Game 2</a>
+		
+</div>
+
+
+</body>
+</html>
